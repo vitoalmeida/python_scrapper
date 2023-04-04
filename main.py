f140fc6f8cb93fa378811b7ac2c7dc0ad5abc101
Vitor Machado <vitoralmeida.work@gmail.com>
2023-04-03 23:49:40 -0300

    version_1

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import tempfile
 import shutil
 import mistune
 import chardet
+import logging
+import datetime
 from github import Github
 from git import Repo
 from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s - %(asctime)s]: %(message)s', datefmt='%H:%M:%S')
+
+ignored_files_extensions = ('.git', '.exe')
 
 
 def clone_repository(github_token, repo_name):
@@ -23,19 +30,35 @@ def clone_repository(github_token, repo_name):
 def convert_files_to_markdown(repo_dir):
     markdown_renderer = mistune.create_markdown()
 
+    readme_content = None
+    markdown_content = ""
+
     markdowns_folder = os.path.join(os.getcwd(), 'markdowns')
 
     if os.path.exists(markdowns_folder):
-        print("Deleting existing markdowns folder...")
+        logging.info("Deleting existing markdowns folder...")
         shutil.rmtree(markdowns_folder)
 
-    print("Creating markdowns folder...")
+    logging.info("Creating markdowns folder...")
     os.makedirs(markdowns_folder, exist_ok=True)
 
-    print("Converting files to Markdown...")
+    logging.info("Fetching README.md...")
+    readme_file_path = os.path.join(repo_dir, 'README.md')
+    if os.path.exists(readme_file_path):
+        with open(readme_file_path, 'r', encoding='utf-8') as f:
+            readme_content = f.read()
+
+    logging.info(
+        "Looping through all files and folders and corveting to markdown...")
     for root, dirs, files in os.walk(repo_dir):
+        # Ignore any folders with ignored names
+        dirs[:] = [d for d in dirs if d not in ignored_files_extensions]
         for file in files:
             file_path = os.path.join(root, file)
+
+            # Check if file extension is ignored
+            if file.endswith(ignored_files_extensions) or (file.lower() == 'readme.md' and root == repo_dir):
+                continue
 
             # Detect the file encoding
             with open(file_path, 'rb') as f:
@@ -46,19 +69,24 @@ def convert_files_to_markdown(repo_dir):
             with open(file_path, 'r', encoding=encoding, errors='ignore') as f:
                 content = f.read()
 
-            markdown_content = markdown_renderer(content)
-
             # Compute the relative path of the file
-            relative_path = os.path.relpath(root, repo_dir)
-            if relative_path == '.':
-                relative_path = ''
+            relative_path = os.path.relpath(file_path, repo_dir)
 
-            # Save the Markdown file in the /markdowns folder with the same relative path
-            md_file_path = os.path.join(
-                markdowns_folder, relative_path, os.path.splitext(file)[0] + '.md')
-            os.makedirs(os.path.dirname(md_file_path), exist_ok=True)
-            with open(md_file_path, 'w', encoding='utf-8') as md_file:
-                md_file.write(markdown_content)
+            # Save the file content in Markdown format
+            markdown_content += f"\n\n### FILE: {relative_path}\n"
+            markdown_content += "```sh\n"
+            markdown_content += content
+            markdown_content += "\n```\n"
+
+    logging.info("Adding readme content to single_markdown file beginning...")
+    if readme_content:
+        markdown_content = f"### README\n\n{readme_content}\n\n" + \
+            markdown_content
+
+    # Save the Markdown content to a file
+    markdown_file_path = os.path.join(markdowns_folder, 'single_markdown.md')
+    with open(markdown_file_path, 'w', encoding='utf-8') as md_file:
+        md_file.write(markdown_content)
 
 
 def remove_readonly(func, path, _):
@@ -68,7 +96,7 @@ def remove_readonly(func, path, _):
 
 def main():
     try:
-        print("Running script...")
+        logging.info("Running script...")
         load_dotenv()
 
         access_token = os.environ["GITHUB_TOKEN"]
@@ -78,14 +106,25 @@ def main():
         # Clone the repository
         cloned_repo = clone_repository(access_token, repo_name)
 
-        # Convert all files in the repository to Markdown
+        # Convert all files in the repository to a single Markdown
         convert_files_to_markdown(cloned_repo.working_dir)
+
+        markdown_file_path = os.path.join(
+            os.getcwd(), 'markdowns', 'single_markdown.md')
+
+        # Test if the Markdown file was generated
+        if os.path.exists(markdown_file_path):
+            logging.info(
+                "\033[92mMarkdown single file successfully generated!\033[0m")
+        else:
+            logging.error(
+                "\033[91mFailed to generate markdown single file!\033[0m")
 
         # Cleanup temporary directory
         shutil.rmtree(cloned_repo.working_dir, onerror=remove_readonly)
 
     except Exception as e:
-        print(f"Error: {str(e)}")
+        logging.info(f"Error: {str(e)}")
 
 
 if __name__ == "__main__":
