414cb18f5a830c6429805739ddc0d5c6495d8d9d
Vitor Machado <vitoralmeida.work@gmail.com>
2023-04-03 22:41:15 -0300

    tests

---
+++ tests/branch/leaf_two.js
@@ -0,0 +1,16 @@
+// This is a comment in JavaScript
+
+// Declare a variable called "message" and assign it a string value
+var message = "Hello, world!";
+
+// Use console.log() to print the value of the "message" variable to the console
+console.log(message);
+
+// Declare a function called "addNumbers" that takes two arguments and returns their sum
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Call the "addNumbers" function with two numbers and print the result to the console
+var result = addNumbers(3, 5);
+console.log("The sum of 3 and 5 is " + result);
